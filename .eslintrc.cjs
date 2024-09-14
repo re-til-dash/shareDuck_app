38e70b2da03e62981d19be57039e971e68b492fd
JiHyeon Lee <justjh30@gmail.com>
2024-09-14 14:15:12 +0900

    [Duck 52] 일렉트론 기본 세팅, 라우터 설정 추가  (#1)
    * 경로 alias 설정 완료
    
    * 패키지 수정 및 라이브러리 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,36 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+      },
+      alias: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+        map: [
+          ["@components", "./src/components"],
+          ["@pages", "./src/pages"],
+          ["@router", "./src/router"],
+          ["@store", "./src/store"],
+          ["@styles", "./src/styles"],
+          ["@types", "./src/types"],
+          ["@utils", "./src/utils"],
+        ],
+      },
+    },
+  },
+};
