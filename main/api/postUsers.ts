6b34e749ae39cdb623405766e65e872c4b011364
Sleepingoff <justjh30@gmail.com>
2024-09-21 12:40:02 +0900

    폴더 구조 변경

---
+++ main/api/postUsers.ts
@@ -0,0 +1,13 @@
+import api from "../config/api.config.ts";
+
+interface typeNewUser {
+  email: string;
+  name: string;
+  password: string;
+  profile: string;
+}
+
+export default async function postUsers(data: typeNewUser) {
+  const config = {};
+  return api.post("/api/v1/users", data, config);
+}
