c98c588621512cf45e588fb77fc59e39ea8ecd09
Sleepingoff <justjh30@gmail.com>
2024-09-22 19:23:44 +0900

    환경변수 변경으로 인한 수정

--- main/config/api.config.ts
+++ main/config/api.config.ts
@@ -3,16 +3,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_BASEAPI_KEY,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json  charset=utf-8",
   },
 });
-
+const baseURL = import.meta.env.VITE_BASE_KEY;
+api.defaults.baseURL = baseURL;
 // 모든 요청은 시간 초과 전 2.5초 대기하는 인스턴스를 사용합니다.
 //api.defaults.timeout = 2500;
 
-//요청 인터셉터
+// 요청 인터셉터
 api.interceptors.request.use(
   function (config: any) {
     // 요청이 전달되기 전에 작업 수행
@@ -26,18 +26,18 @@ api.interceptors.request.use(
 );
 
 // 응답 인터셉터
-api.interceptors.response.use(
-  function (response: any) {
-    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-    // 응답 데이터가 있는 작업 수행
-    return response;
-  },
-  function (error: any) {
-    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-    // 응답 오류가 있는 작업 수행
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.response.use(
+//   function (response: any) {
+//     // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+//     // 응답 데이터가 있는 작업 수행
+//     return response;
+//   },
+//   function (error: any) {
+//     // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+//     // 응답 오류가 있는 작업 수행
+//     return Promise.reject(error);
+//   }
+// );
 
 // 인터셉터 제거 (필요 시)
 // const myInterceptor = axios.interceptors.request.use(function () {
