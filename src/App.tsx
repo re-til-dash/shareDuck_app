d3be9b80225109e979078027a1ddbb1f0bdea31c
Sleepingoff <justjh30@gmail.com>
2024-09-22 19:25:59 +0900

    필요없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import Layout from "@components/template/Layout";
-import { useEffect } from "react";
 import { GlobalStyle, LightTheme } from "shareduck-ui";
 import { ThemeProvider } from "styled-components";
 function App() {
-  useEffect(() => {
-    window.shareDuck
-      .invoke("categories-get-ipc")
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  }, []);
   return (
     <ThemeProvider theme={LightTheme}>
       <GlobalStyle />
