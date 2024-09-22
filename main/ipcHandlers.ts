62d931201bf67b9798de2bd429b35e426635e251
Sleepingoff <justjh30@gmail.com>
2024-09-22 19:24:29 +0900

    필요없는 코드 삭제 및 타입 정의 추가

--- main/ipcHandlers.ts
+++ main/ipcHandlers.ts
@@ -2,18 +2,14 @@
 
 import { ipcMain } from "electron";
 import handlers, { typeHandlers } from "./config/handlers.config.ts";
-// 'get-user-data' 채널에 대한 핸들러 등록
-ipcMain.handle("get-user-data", async (event, userId) => {
-  // 메인 프로세스에서 처리할 로직
-  const userData = { id: userId, name: "John Doe", email: "john@example.com" };
 
-  // 결과를 렌더러 프로세스에 반환
-  return userData;
-});
-
-export default function registerIpcHandler(method: string, key: typeHandlers) {
+export type typeMethod = "get" | "post" | "patch" | "delete";
+export default function registerIpcHandler(
+  method: typeMethod,
+  key: typeHandlers
+) {
   ipcMain.handle(`${key}-${method}-ipc`, async (_e, ..._args) => {
     const handler = handlers[key]?.[method];
-    return await handler(..._args);
+    return await handler(...(_args as [any]));
   });
 }
