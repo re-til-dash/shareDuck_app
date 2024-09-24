e8eee82e3ee876e09b250f5ed9eaac92380c641e
JiHyeon Lee <justjh30@gmail.com>
2024-09-24 12:37:02 +0900

    [Duck 12]  윈도우용 statusbar 추가 (#9)
    * 윈도우용 헤더 컴포넌트 추가
    
    * 헤더 기본 이벤트 추가
    
    * 경로 오류 수정
    
    * 사이드바 위치 수정
    
    * UI 라이브러리 수정

--- main/main.ts
+++ main/main.ts
@@ -1,10 +1,11 @@
 //메인 프로세스 진입점 파일
 
-import { app } from "electron";
+import { app, ipcMain, screen } from "electron";
 
 import createWindow from "./windows/windows.ts";
 import registerIpcHandler, { typeMethod } from "./ipcHandlers.ts";
 import handlers, { typeHandlers } from "./config/handlers.config.ts";
+import { WINDOW_DEFAULT_SIZE } from "./config/window.config.ts";
 const methdos: typeMethod[] = ["get", "post", "patch", "delete"];
 function initializeApp() {
   const keys = Object.keys(handlers) as typeHandlers[];
@@ -12,7 +13,39 @@ function initializeApp() {
     keys.forEach((key) => registerIpcHandler(method, key));
   });
 
-  createWindow();
+  const mainWindow = createWindow();
+
+  ipcMain.on("title-bar-action", (_e, action) => {
+    switch (action) {
+      case "MIN": {
+        mainWindow.setSize(
+          WINDOW_DEFAULT_SIZE.width,
+          WINDOW_DEFAULT_SIZE.height
+        );
+        break;
+      }
+      case "MAX": {
+        mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        break;
+      }
+      case "CLOSE_MIN": {
+        mainWindow.minimize();
+        break;
+      }
+      case "CLOSE": {
+        mainWindow.close();
+        //!!개발모드에서만 다음코드 사용
+        app.quit();
+        break;
+      }
+      default:
+        mainWindow.setSize(
+          WINDOW_DEFAULT_SIZE.width,
+          WINDOW_DEFAULT_SIZE.height
+        );
+    }
+  });
+
   app.setName("shareDuck");
 }
 
