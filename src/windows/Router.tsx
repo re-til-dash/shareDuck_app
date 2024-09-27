d6aeeaeb0c5f24cd5eb072142537a9982c5b38d7
Sleepingoff <justjh30@gmail.com>
2024-09-27 15:38:28 +0900

    카테고리 컴포넌트 이름 오타 수정

--- src/windows/Router.tsx
+++ src/windows/Router.tsx
@@ -2,7 +2,7 @@ import App from "../App";
 import { createHashRouter, RouteObject } from "react-router-dom";
 import Home from "@pages/Home";
 import Error from "./Error";
-import Categroy from "@pages/Category";
+import Category from "@pages/Category";
 import Post from "@pages/Post";
 import Overview from "@pages/Overview";
 
@@ -26,7 +26,7 @@ const routers: RouteObject[] = [
       { path: "/home", element: <Home /> },
       {
         path: "/:menu",
-        element: <Categroy />,
+        element: <Category />,
         children: [
           {
             path: "/:menu/post",
