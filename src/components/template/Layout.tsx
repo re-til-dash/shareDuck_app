8c0fbe090bffb4e117af8507b65743a7c611cc5c
jhlee <jhlee@cytur.net>
2024-09-28 18:30:27 +0900

    feat: add upload img api,

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -5,7 +5,7 @@ function Layout() {
   return (
     <StyledMain>
       <Sidebar />
-      <div style={{ width: "117px" }}></div>
+      <div style={{ width: "212.7px" }}></div>
       <Outlet />
     </StyledMain>
   );
