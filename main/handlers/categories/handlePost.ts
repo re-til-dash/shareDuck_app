b6a42e90530df43dafab8382da167bf0977af5f9
Sleepingoff <justjh30@gmail.com>
2024-10-03 22:07:36 +0900

    카테고리 추가 이슈 해결

--- main/handlers/categories/handlePost.ts
+++ main/handlers/categories/handlePost.ts
@@ -10,15 +10,20 @@ const FILE_NAME = "categories.json";
 
 export default async function handlePostCategories(data) {
   const filePath = path.join(userDataPath, FOLDER_NAME, FILE_NAME);
-  // 2. 파일이 존재하는지 확인
+  // 1. API 호출
+  let result = await postCategories(data);
+  //에러 메세지로 들어오는 경우
+  if (typeof result === "string") {
+    throw Error(result);
+  }
+
   if (hasLocalFile(FOLDER_NAME, FILE_NAME)) {
+    // 2. 파일이 존재하는지 확인
     try {
       // 3. 파일이 있으면 파일에서 기존 데이터 불러오기
       const fileContent = fs.readFileSync(filePath, "utf8");
       const prevData = JSON.parse(fileContent);
-      //!카테고리의 순서를 옮겨야 한다면 여기를 고쳐야함!
-      data.id = prevData.length;
-      prevData.push(data);
+      prevData.push(result);
       // 기존 데이터에 새로운 데이터 추가하기
       fs.writeFileSync(filePath, JSON.stringify(prevData));
       return prevData;
@@ -26,13 +31,6 @@ export default async function handlePostCategories(data) {
       console.error("Error reading local file:", err);
       // 파일을 읽는 중 오류가 발생하면 이후 로직
     }
-  }
-  // 1. API 호출
-  let result = await postCategories(data);
-
-  //에러 메세지로 들어오는 경우 바로 메세지 반환
-  if (typeof result === "string") {
-    return result;
   }
 
   // 4. 로컬에 파일이 없으므로 새로 생성
